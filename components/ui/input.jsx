2326fef4d25e3314aeb3798afd3d4796701737f1
anilkumar70820 <ak9565191@gmail.com>
2024-04-06 10:46:46 +0530

    ad d input

--- components/ui/input.jsx
+++ components/ui/input.jsx
@@ -7,7 +7,7 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
     (<input
       type={type}
       className={cn(
-        " w-full rounded-md bg-[#171717] px-[18px] py-[11px] text-sm file:text-sm file:font-medium placeholder:text-gray-500",
+        " w-full rounded-[14px] bg-[#171717] px-[18px] py-[11px] text-sm text-[#E1E1E1] file:text-sm file:font-medium placeholder:text-gray-500",
         className
       )}
       ref={ref}
