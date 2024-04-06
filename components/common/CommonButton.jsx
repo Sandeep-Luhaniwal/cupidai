b0eca009fa78908e287f7dc9839aa782d3bddcfb
Sandeep-Luhaniwal <sandeepluh0001.com>
2024-04-06 10:18:42 +0530

    first commit

---
+++ components/common/CommonButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+const CommonButton = () => {
+    return (
+        <div>
+            <Button>Button</Button>
+        </div>
+    )
+}
+
+export default CommonButton
---
+++ components/common/CommonCheckBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Checkbox } from '../ui/checkbox'
+
+const CommonCheckBox = () => {
+    return (
+        <div>
+            <Checkbox />
+        </div>
+    )
+}
+
+export default CommonCheckBox
